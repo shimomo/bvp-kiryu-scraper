8a19fece4ea9de0026d04f8848e732d4d0812dc2
shimomo <yuichi@shimomo.net>
2025-05-05 22:18:04 +0900

    style: Clean up code style

--- src/Scrapers/TimeScraper.php
+++ src/Scrapers/TimeScraper.php
@@ -52,11 +52,11 @@ class TimeScraper extends BaseScraper implements TimeScraperInterface
             $racerTurnTime = Normalizer::normalize($times['.col7'][$boatNumber] ?? 0.0);
             $racerStraightTime = Normalizer::normalize($times['.col8'][$boatNumber - 1] ?? 0.0);
 
-            $response['boat_number_' . $boatNumber . '_racer_name'] = $racerName;
-            $response['boat_number_' . $boatNumber . '_racer_exhibition_time'] = $racerExhibitionTime;
-            $response['boat_number_' . $boatNumber . '_racer_half_lap_time'] = $racerHalfLapTime;
-            $response['boat_number_' . $boatNumber . '_racer_turn_time'] = $racerTurnTime;
-            $response['boat_number_' . $boatNumber . '_racer_straight_time'] = $racerStraightTime;
+            $response["boat_number_{$boatNumber}_racer_name"] = $racerName;
+            $response["boat_number_{$boatNumber}_racer_exhibition_time"] = $racerExhibitionTime;
+            $response["boat_number_{$boatNumber}_racer_half_lap_time"] = $racerHalfLapTime;
+            $response["boat_number_{$boatNumber}_racer_turn_time"] = $racerTurnTime;
+            $response["boat_number_{$boatNumber}_racer_straight_time"] = $racerStraightTime;
         }
 
         return $response;
