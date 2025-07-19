e3628c8fccaeec8e1aa3d3fae91b25bc0a80c297
shimomo <yuichi@shimomo.net>
2025-07-20 00:41:13 +0900

    test: Rename `testScrapeForecastsWithRaceCode1AndDate20240108()` to `testThrowsExceptionWhenKeyNotFoundInForecasts()`

--- tests/ScraperTest.php
+++ tests/ScraperTest.php
@@ -53,7 +53,7 @@ final class ScraperTest extends TestCase
     /**
      * @return void
      */
-    public function testScrapeForecastsWithRaceCode1AndDate20240108(): void
+    public function testThrowsExceptionWhenKeyNotFoundInForecasts(): void
     {
         $this->expectException(\RuntimeException::class);
         $this->expectExceptionMessage(
